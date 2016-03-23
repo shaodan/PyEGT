0a1d5b36c77e90cadff0a6f98f0aa6738d323dd4
ShaoDan <shaodan.cn@gmail.com>
2016-03-23 11:27:39 +0800

    利用局部更新，加速计算博弈收益

--- game.py
+++ game.py
@@ -11,56 +11,75 @@ class Game(object):
         pass
 
     # 博弈过程，必须继承
-    def play(self, graph, strategy, fitness):
-        pass
+    def play(self, graph, strategy, fitness, node_list=None, edge_list=None):
+        if node_list is None:
+            # 初始化fitness，整体进行一次博弈
+            pass
+        elif node_list:
+            # 局部更新
+            self.fast_play(graph, strategy, fitness, node_list, edge_list)
 
-    def fast_play(self, graph, strategy, fitness, node_list=None, edge_list=None):
-        pass
+    def fast_play(self, graph, strategy, fitness, node_list, edge_list=None):
+        # todo : 会有精度差别 10^-16~-15数量级
+        if not isinstance(node_list, list):
+            # 先转换成list
+            node_list = [node_list]
+        for node in node_list:
+            pass
 
 
 class PGG(Game):
     """public_goods_game"""
 
-    def __init__(self, r=3):
+    def __init__(self, r=2):
         super(self.__class__, self).__init__()
         # 获利倍数
-        self.r = r
+        self.r = float(r)
 
-    def play(self, graph, strategy, fitness):
+    def play(self, graph, strategy, fitness, node_list=None, edge_list=None):
         # 可能会有负的fitness
-        fitness.fill(0)
-        # 第一种每个group投入1
-        degrees = np.array(graph.degree().values())
-        for node in graph.nodes_iter():
-            degree = degrees[node]
-            fitness[node] += (strategy[node] - 1) * (degree+1)
-            neighs = graph.neighbors(node)
-            neighs.append(node)
-            # b = self.r * (strategy[neighs] == 0).sum() / float(degree+1)
-            b = self.r * (len(neighs) - np.count_nonzero(strategy[neighs])) / float(degree+1)
-            for neigh in neighs:
-                fitness[neigh] += b
-        # 第二种每个group投入1/(k+1)
-        # degrees = np.array(G.degree().values())
-        # inv = (1.0-s) / (degrees)
-        # for node in G.nodes_iter():
-        #     fitness[node] += s[node] - 1
-        #     neighs = G.neighbors(node)
-        #     neighs.append(node)
-        #     b = self.r * inv[neighs].sum() / float(degrees[node]+1)
-        #     for neigh in neighs:
-        #         fitness[neigh] += b
-
-    def fast_play(self, graph, strategy, fitness, node_list=None, edge_list=None):
-        if not node_list:
-            self.play(graph, strategy, fitness)
-        elif not isinstance(node_list, list):
-            self.fast_play(graph, strategy, fitness, [node_list], edge_list)
+        if node_list is None:
+            fitness.fill(0)
+            # 第一种每个group投入1
+            degrees = np.array(graph.degree().values())
+            for node in graph.nodes_iter():
+                degree = degrees[node]
+                fitness[node] += (strategy[node] - 1) * (degree+1)
+                neighs = graph.neighbors(node)
+                neighs.append(node)
+                # b = self.r * (strategy[neighs] == 0).sum() / (degree+1)
+                b = self.r * (len(neighs) - np.count_nonzero(strategy[neighs])) / (degree+1)
+                for neigh in neighs:
+                    fitness[neigh] += b
+            # 第二种每个group投入1/(k+1)
+            # degrees = np.array(G.degree().values())
+            # inv = (1.0-s) / (degrees)
+            # for node in G.nodes_iter():
+            #     fitness[node] += s[node] - 1
+            #     neighs = G.neighbors(node)
+            #     neighs.append(node)
+            #     b = self.r * inv[neighs].sum() / float(degrees[node]+1)
+            #     for neigh in neighs:
+            #         fitness[neigh] += b
         else:
-            for node in node_list:
-                fitness[node] = 0
-                for neigh in graph.neighbors_iter(node):
-                    fitness[node] += 1
+            self.fast_play(graph, strategy, fitness, node_list)
+
+    def fast_play(self, graph, strategy, fitness, node_list, edge_list=None):
+        if not isinstance(node_list, list):
+            node_list = [node_list]
+        for node in node_list:
+            s = strategy[node]
+            sign = (1 - 2*s)
+            sign_r = sign * self.r
+            d = graph.degree(node)
+            # 更新节点作为中心pgg产生的收益增量
+            delta = sign_r/(d+1)
+            fitness[node] += delta - sign*(d+1)
+            for neigh in graph.neighbors_iter(node):
+                delta_neigh = sign_r/(graph.degree(neigh)+1)
+                fitness[neigh] += delta + delta_neigh
+                for neigh_neigh in graph.neighbors_iter(neigh):
+                    fitness[neigh_neigh] += delta_neigh
 
 
 class PDG(Game):
@@ -70,36 +89,36 @@ class PDG(Game):
         super(self.__class__, self).__init__()
         self.payoff = np.array([[(r, r), (s, t)], [(t, s), (p, p)]], dtype=np.double)
 
-    def play(self, graph, strategy, fitness):
-        fitness.fill(0)
-        for edge in graph.edges_iter():
-            a = edge[0]
-            b = edge[1]
-            p = self.payoff[strategy[a]][strategy[b]]
-            fitness[a] += p[0]
-            fitness[b] += p[1]
-
-    def fast_play(self, graph, strategy, fitness, node_list=None, edge_list=None):
-        if not node_list:
-            self.play(graph, strategy, fitness)
-        elif not isinstance(node_list, list):
-            self.fast_play(graph, strategy, fitness, [node_list], edge_list)
-        elif True:
-            # 只用计算新节点和其邻居节点的收益
-            for node in node_list:
-                f = 0  # 新节点收益从0计算
-                for neigh in graph.neighbors_iter(node):
-                    p = self.payoff[strategy[node]][strategy[neigh]]
-                    f += p[0]           # 新节点累加
-                    new_payoff = p[1]   # 邻居节点计算新的收益
-                    # 0合作，1背叛
-                    p = self.payoff[1-strategy[node]][strategy[neigh]]
-                    old_payoff = p[1]   # 邻居节点计算原来的收益
-                    fitness[neigh] += new_payoff - old_payoff
-                fitness[node] = f
+    def play(self, graph, strategy, fitness, node_list=None, edge_list=None):
+        if node_list is None:
+            fitness.fill(0)
+            for edge in graph.edges_iter():
+                a = edge[0]
+                b = edge[1]
+                p = self.payoff[strategy[a]][strategy[b]]
+                fitness[a] += p[0]
+                fitness[b] += p[1]
         else:
-            # 节点策略没有变化
-            pass
+            self.fast_play(graph, strategy, fitness, node_list)
+
+    def fast_play(self, graph, strategy, fitness, node_list, edge_list=None):
+        if not isinstance(node_list, list):
+            node_list = [node_list]
+        # 只用计算新节点和其邻居节点的收益
+        # 如果node_list为空list，则不更新
+        for node in node_list:
+            f = 0  # 新节点收益从0计算
+            s = strategy[node]
+            s_ = 1 - s
+            for neigh in graph.neighbors_iter(node):
+                p = self.payoff[s][strategy[neigh]]
+                f += p[0]           # 新节点累加
+                new_payoff = p[1]   # 邻居节点计算新的收益
+                # 0合作，1背叛
+                p = self.payoff[s_][strategy[neigh]]
+                old_payoff = p[1]   # 邻居节点计算原来的收益
+                fitness[neigh] += new_payoff - old_payoff
+            fitness[node] = f
 
 
 class RPG(Game):
@@ -109,15 +128,35 @@ class RPG(Game):
         super(self.__class__, self).__init__()
         self.ration = ration
 
-    def play(self, graph, strategy, fitness):
+    def play(self, graph, strategy, fitness, node_list=None, edge_list=None):
         pass
 
 
 # TEST CODE HERE
 if __name__ == '__main__':
-    g = PDG()
-    G = nx.random_regular_graph(5, 10)
-    st = np.random.randint(2, size=10)
-    fit = np.empty(10)
+    # g = PDG()
+    g = PGG(2)
+    G = nx.random_regular_graph(5, 100)
+    st = np.random.randint(2, size=100)
+    fit = np.empty(100)
     g.play(G, st, fit)
     print fit
+
+    i = np.random.randint(100)
+    st[i] = 1-st[i]
+    fit1 = fit.copy()
+    fit2 = fit.copy()
+    print "=======fit1==========="
+    print fit1
+    print "=======fit2==========="
+    print fit2
+    g.play(G, st, fit1, i)
+    g.play(G, st, fit2)
+
+    print "=======delt==========="
+    print (fit1-fit2)
+    print "=======sum==========="
+    print (fit1-fit2).sum()
+
+    print (fit1[i] - fit2[i])
+    print G.degree(i)
